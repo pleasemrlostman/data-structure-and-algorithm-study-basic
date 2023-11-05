e8bfab71335364537c21e5bcb3ee7a42b5b92fe4
정재훈 <70499531+pleasemrlostman@users.noreply.github.com>
2023-11-06 02:52:39 +0900

    first commit

---
+++ test.mjs
@@ -0,0 +1,22 @@
+import { Node, LinkedList } from "./LinkedList.mjs";
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+
+node1.next = node2;
+node2.next = node3;
+
+console.log(node1);
+console.log(node2);
+console.log(node3);
+console.log(node1.data);
+console.log(node1.next.data);
+console.log(node1.next.next.data);
+
+let list = new LinkedList();
+console.log("======= insertAt() 다섯 번 호출 =======");
+list.insertAt(0, 0);
+list.insertAt(1, 1);
+list.printAll();
+console.log("list", list);
