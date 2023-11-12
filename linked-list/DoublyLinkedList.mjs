e242dc904b220e674b24ff0fb2457b9272a99489
정재훈 <70499531+pleasemrlostman@users.noreply.github.com>
2023-11-12 18:22:07 +0900

    📝 [DOCS](custom): stack / queue / dequeue 구현 및 문서작성

--- linked-list/DoublyLinkedList.mjs
+++ linked-list/DoublyLinkedList.mjs
@@ -1,13 +1,15 @@
 class Node {
-  constructor(data, next = null) {
+  constructor(data, next = null, prev = null) {
     this.data = data;
     this.next = next;
+    this.prev = prev; // 이전값도 받아야 하기 때문에 prev 프로퍼티 추가
   }
 }
 
-class LinkedList {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
+    this.tail = null; // 큐를 구현하기 위해 리스트의 끝을 가리키는 프로퍼티도 추가
     this.count = 0;
   }
 
@@ -40,15 +42,31 @@ class LinkedList {
     let newNode = new Node(data);
     if (index == 0) {
       newNode.next = this.head;
+      if (this.head !== null) {
+        this.head.prev = newNode; // 만약 헤드에 추가한다면 기존의 헤드의 prev값을 새로 추가한 노드 값으으로 할당해준다
+      }
       this.head = newNode;
+    } else if (index == this.count) {
+      // 맨 마지막에 삽입하는 경우도 추가 해줘야함
+      newNode.next = null;
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
     } else {
       let currentNode = this.head;
       for (let i = 0; i < index - 1; i++) {
         currentNode = currentNode.next;
       }
       newNode.next = currentNode.next;
+      newNode.prev = currentNode; // 새로 삽입한 노드의 이전을 현재 노드로 해주고 (반복문을 -1 만큼 돌리기 때문)
+
       currentNode.next = newNode;
+      newNode.next.prev = newNode; // 새로 삽입한 노드의 이전 노드를 새로 삽입한 노드로 변경해준다
     }
+
+    if (newNode.next == null) {
+      this.tail = newNode;
+    }
+
     this.count++;
   }
 
@@ -64,7 +82,21 @@ class LinkedList {
     let currentNode = this.head;
     if (index == 0) {
       let deletedNode = this.head;
-      this.head = this.head.next;
+      if (this.head.next == null) {
+        // head와 tail이 똑같은 경우
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head.next;
+        this.head.prev = null;
+      }
+      this.count--;
+      return deletedNode;
+    } else if (index == this.count - 1) {
+      let deletedNode = this.tail;
+
+      this.tail.prev.next = null;
+      this.tail = this.tail.prev;
       this.count--;
       return deletedNode;
     } else {
@@ -73,6 +105,7 @@ class LinkedList {
       }
       let deletedNode = currentNode.next;
       currentNode.next = currentNode.next.next;
+      currentNode.next.prev = currentNode;
       this.count--;
       return deletedNode;
     }
@@ -94,4 +127,4 @@ class LinkedList {
   }
 }
 
-export { Node, LinkedList };
+export { Node, DoublyLinkedList };
---
+++ stack/test_stack.mjs
@@ -0,0 +1,27 @@
+import { Stack } from "./Stack.mjs";
+
+let stack = new Stack();
+
+console.log("=============첫 번쨰 출력 ===============");
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+console.log(stack.pop().data);
+console.log(stack.pop().data);
+console.log(stack.pop().data);
+console.log(stack.pop().data);
+console.log("=============두 번쨰 출력 ===============");
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+console.log(stack.peek().data);
+stack.pop();
+console.log(stack.peek().data);
+console.log(`is Empty: ${stack.isEmpty()}`);
+stack.pop();
+stack.pop();
+stack.pop();
+console.log(`is Empty: ${stack.isEmpty()}`);
+console.log(stack.pop());
